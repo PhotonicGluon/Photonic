3756c9237d1a35b225e4ed47e7a907e5f36c4791
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-12-31 15:15:45 +0800

    Add tailwind dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,11 @@ import glsl from "vite-plugin-glsl";
 import photonicTweakpane from "./lib/tweakpane/dev-tools/integration";
 
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [photonicTweakpane, preact()],
+    integrations: [photonicTweakpane, preact(), tailwind()],
     vite: {
         plugins: [glsl()],
     },
