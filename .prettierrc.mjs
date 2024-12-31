87626b9611bda8f0e6b2504f7da21493c898eb66
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-12-31 16:38:29 +0800

    Convert all styles to use TailwindCSS

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,6 +1,6 @@
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ["prettier-plugin-astro"],
+    plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
     tabWidth: 4,
     printWidth: 120,
     overrides: [
@@ -11,4 +11,5 @@ export default {
             },
         },
     ],
+    tailwindConfig: "./tailwind.config.mjs"
 };
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 export default {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+        fontFamily: {
+            sans: ["DM Sans", "Segoe UI", "Roboto", "Helvetica Neue", "Arial", "sans-serif"],
+            mono: ["JetBrains Mono", "SFMono-Regular", "Menlo", "Monaco", "Consolas", "Courier New", "monospace"],
+        },
     },
     plugins: [],
 };
