3210b1fbc439a6fd7e16a7de0077f55a402e8936
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-01-01 16:13:02 +0800

    Make `tags` use `ProjectTag` enum

--- lib/projects/schema.ts
+++ lib/projects/schema.ts
@@ -1,5 +1,17 @@
 import { z } from "astro:content";
 import type { ZodSchema } from "astro:schema";
+import { ProjectTag } from "./structures";
+
+/**
+ * Gets all project tags from the `ProjectTag` enum.
+ *
+ * @returns List of project tag IDs, in lowercase.
+ */
+function getTags(): readonly [string, ...string[]] {
+    let tags = Object.keys(ProjectTag);
+    tags = tags.map((tag) => tag.toLowerCase());
+    return tags as [string, ...string[]];
+}
 
 /**
  * Generates the Zod schema for project information.
@@ -14,7 +26,7 @@ export default async function generateSchema(): Promise<ZodSchema> {
             start: z.coerce.date(),
             end: z.coerce.date().optional(),
         }),
-        tags: z.array(z.enum(["mathematics", "music", "programming", "writing"])), // TODO: Use predefined tags
+        tags: z.array(z.enum(getTags())),
         banner: z.string().optional(),
         urls: z.object({
             bandcamp: z.string().optional(),
