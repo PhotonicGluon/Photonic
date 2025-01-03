b485d23e04f060839b725475e77c252edb86a7ca
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-01-03 20:37:42 +0800

    Remove preact dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import glsl from "vite-plugin-glsl";
 
 import photonicTweakpane from "./lib/tweakpane/dev-tools/integration";
 
-import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [photonicTweakpane, preact(), tailwind()],
+    integrations: [photonicTweakpane, tailwind()],
     vite: {
         plugins: [glsl()],
     },
