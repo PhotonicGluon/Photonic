2f8bb8e2a0dca968c9d80a3384b50600e13ea475
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-01-18 18:29:07 +0800

    Add preact dependency
    We need astro islands for filtering/sorting/searching of projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import glsl from "vite-plugin-glsl";
 
 import photonicTweakpane from "./lib/tweakpane/dev-tools/integration";
 
+import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [photonicTweakpane, tailwind()],
+    integrations: [photonicTweakpane, preact(), tailwind()],
     vite: {
         plugins: [glsl()],
     },
