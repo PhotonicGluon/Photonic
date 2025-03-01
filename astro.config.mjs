58f7725c2c67f3de8e7ac6e9b30d8995d0e0bb7d
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-03-01 14:28:07 +0800

    Add astro sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "astro/config";
 
 import photonicTweakpane from "./lib/tweakpane/dev-tools/integration";
 import preact from "@astrojs/preact";
+import sitemap from "@astrojs/sitemap";
 
 import glsl from "vite-plugin-glsl";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [photonicTweakpane, preact()],
+    site: "https://photonic.dev",
+    integrations: [photonicTweakpane, preact(), sitemap()],
     vite: {
         plugins: [glsl(), tailwindcss()],
     },
