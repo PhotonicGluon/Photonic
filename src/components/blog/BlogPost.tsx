e28124b555bad768ea83056b7d9b8b1f1ef3d524
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-03-15 18:36:12 +0800

    Convert blog post component to island

---
+++ src/components/blog/BlogPost.tsx
@@ -0,0 +1,52 @@
+import { Component } from "preact";
+
+import type { Post } from "@lib/blog/post";
+import { humanizeDate } from "@lib/misc/dates";
+
+interface Props {
+    /** Post object to display on the list of posts */
+    post: Post;
+}
+
+interface State {}
+
+export default class BlogPost extends Component<Props, State> {
+    render(props: Props, state: State) {
+        const post = props.post;
+        const frontmatter = post.frontmatter;
+
+        return (
+            <a href={post.url} class="*:text-white">
+                <article class="blog-post flex flex-col border border-gray-700 bg-gray-900 transition-transform duration-300 ease-out hover:scale-[1.05] hover:cursor-pointer">
+                    {/* Preamble */}
+                    <div class="mx-2 mt-2 md:mx-4 md:mt-4">
+                        {/* Publication date */}
+                        <span class="font-mono text-sm">{humanizeDate(new Date(frontmatter.pubDate))}</span>
+
+                        {/* TODO: Add tags */}
+                    </div>
+
+                    {/* Blog cover image */}
+                    {/* TODO: Can we use Astro's Image component? */}
+                    {frontmatter.image && (
+                        <img
+                            class="mt-2 md:mt-4"
+                            src={frontmatter.image.url}
+                            alt={frontmatter.image.alt}
+                            loading="eager"
+                        />
+                    )}
+
+                    {/* Main post content */}
+                    <div class="m-2 grid md:m-4 lg:grid-cols-2">
+                        {/* Title */}
+                        <h1 class="font-mono font-bold md:text-xl lg:text-2xl">{frontmatter.title}</h1>
+
+                        {/* Summary */}
+                        <p class="!p-0 text-gray-300">{frontmatter.summary}</p>
+                    </div>
+                </article>
+            </a>
+        );
+    }
+}
