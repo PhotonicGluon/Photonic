1dc0f0395cd5f86a645b4a3fcb1995f278bcc24d
Ryan Kan <25820201+PhotonicGluon@users.noreply.github.com>
2025-04-28 16:32:00 +0800

    Add modal for citation information

--- lib/misc/dates.ts
+++ lib/misc/dates.ts
@@ -1,3 +1,29 @@
+const MONTHS = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+
+/**
+ * Converts the month index to a string.
+ *
+ * @param month The month index.
+ * @returns Month string.
+ */
+export function strMonth(month: number): string {
+    if (month < 0 || month >= 12) throw Error(`Invalid month index ${month}`);
+    return MONTHS[month];
+}
+
 /**
  * Converts a Date object to a string in the format "YYYY-MM-DD".
  *
@@ -25,4 +51,22 @@ export function humanizeDate(date: Date): string {
         day: "numeric",
     });
     return dateFormat.format(date);
+}
+
+/**
+ * Converts a Date object to the APA7 date format (e.g., "2025, March 14").
+ *
+ * @param date The Date object to format.
+ * @param yearFirst Whether the year should come first in the date or not.
+ * @returns A string representing the date in the APA7 citation format.
+ */
+export function citationDate(date: Date, yearFirst: boolean = true): string {
+    const day = date.getDate();
+    const month = strMonth(date.getMonth());
+    const year = date.getFullYear();
+
+    if (yearFirst) {
+        return `${year}, ${month} ${day}`;
+    }
+    return `${month} ${day}, ${year}`;
 }
