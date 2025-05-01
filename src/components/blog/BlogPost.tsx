9a7c2870f8d00611f011e0732a52d2dcce24aac7
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2025-05-01 14:15:44 +0800

    Fix `tabindex` issue with `BlogPost.tsx`

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -15,8 +15,11 @@ export default class BlogPost extends Component<Props, State> {
         const post = props.post;
 
         return (
-            <a href={"/blog/" + post.slug} class="h-min *:text-white print:*:text-black">
-                <article class="blog-post flex flex-col border border-gray-700 bg-gray-900 transition-transform duration-300 ease-out hover:cursor-pointer hover:not-print:scale-[1.05] print:bg-white">
+            <a
+                href={"/blog/" + post.slug}
+                class="h-min transition-transform duration-300 ease-out *:text-white hover:cursor-pointer hover:not-print:scale-105 focus:scale-105 print:*:text-black"
+            >
+                <article class="blog-post flex flex-col border border-gray-700 bg-gray-900 print:bg-white">
                     {/* Preamble */}
                     <div class="mx-2 mt-2 md:mx-4 md:mt-4">
                         {/* Publication date */}
